60534eea147e2a52bfdf1c552ba60b545f12733e
Maria Clara Duarte <mcduarte9@hotmail.com>
2025-07-24 10:00:05 -0300

    feat: estrutura básica dos componentes

--- formulario_multistep/src/App.tsx
+++ formulario_multistep/src/App.tsx
@@ -1,12 +1,47 @@
-import './App.css'
+// Components
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+
+//Icons
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+
+// Hooks
+
+// CSS
+
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='app'>
-      <h1>React Multistep Form</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para
+          avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>Passos</p>
+        <form>
+          <div className="inputs-container">
+            <UserForm />
+          </div>
+          <div className="actions">
+            <button>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
